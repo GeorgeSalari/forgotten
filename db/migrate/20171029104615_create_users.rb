282db13489afbf5cf0a01b977339e6aec1906fe0
GeorgeSalari <x3m.giku@gmail.com>
2017-10-29 21:56:02 +0700

    fix menu view

--- db/migrate/20171029104615_create_users.rb
+++ db/migrate/20171029104615_create_users.rb
@@ -3,9 +3,13 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
+      t.string :nick_name
       t.integer :role
       t.string :email
       t.string :password_digest
+      t.string :gender
+      t.date :birthday
+      t.string :profile_photo
       t.timestamps
     end
   end
