c8ba48252fac7dd826317604b60d217a9bb664b0
GeorgeSalari <x3m.giku@gmail.com>
2017-11-28 15:16:37 +0700

    add forum_location

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -4,4 +4,12 @@ class Theme < ApplicationRecord
   belongs_to :group
 
   validates :title, presence: true
+
+  def self.set_location(object)
+    $location = ["Форум"]
+    $location << object.group.title
+    $location << object.group.id
+    $location << object.title
+    $location << object.id
+  end
 end
