c40b5108349c42956412f413572121c5f804329b
GeorgeSalari <x3m.giku@gmail.com>
2017-12-19 13:20:43 +0700

    finish with experience calculator

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -13,6 +13,9 @@ class WelcomeController < ApplicationController
   end
 
   def check_experience
-    byebug
+    if params[:current_experience].to_i > 0
+      @result = PlayerLevel.check_gived_experience(params[:current_experience], params[:desired_level])
+    end
+    render :json => @result
   end
 end
